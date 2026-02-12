126d902dec57d11e323cc22a95ce7daf56de3ee9
Majid Naseem <59910150+majidk300@users.noreply.github.com>
2026-02-12 09:23:21 +0530

    Adjust margin-top in Signup component

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
 
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4 mt-15">
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4 mt-8">
         <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
 
           {/* Header */}
